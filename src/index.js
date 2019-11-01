50d7a6906fc07ec7c17ad39ea94977ce3a6640a6
huequica <harumaki1225@gmail.com>
2019-11-01 18:59:13 +0900

    confirmed React is running

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'react-hot-loader/patch';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = () => {
+  // eslint-disable-next-line no-unused-vars
+  function App(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+  
+  ReactDOM.render(<App />,document.querySelector('div#app'));
+}
