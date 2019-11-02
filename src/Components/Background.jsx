57ea2a3907bc4fd7848f7b51f53b9b1594355ce9
huequica <harumaki1225@gmail.com>
2019-11-02 20:21:39 +0900

    make style props

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -3,6 +3,12 @@ import ReactPlayer from 'react-player';
 
 export function Background() {
   return(
-    <ReactPlayer url='https://www.youtube.com/watch?v=YAwoRybL1t0' playing />
+    <ReactPlayer
+     url='https://www.youtube.com/watch?v=YAwoRybL1t0'
+     width='100vw'
+     height='100vh'
+     playing='true'
+     controls='true'
+    />
   );
 }
