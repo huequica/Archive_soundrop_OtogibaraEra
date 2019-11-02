ca3e3b44c6d08baba0eb6ab6fc962fde997fb132
huequica <harumaki1225@gmail.com>
2019-11-02 19:19:00 +0900

    create components file

---
+++ src/Components/Background.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+export function Background() {
+  return(
+    <ReactPlayer url='https://www.youtube.com/watch?v=YAwoRybL1t0' playing />
+  );
+}
